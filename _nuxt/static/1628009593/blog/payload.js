3f6cd582334fef4922202c7c8ca480b0e3bdc446
thombruce <thombruce@users.noreply.github.com>
2021-08-07 16:17:37 +0000

    deploy: febf2a4e822a3db07312d5469df2fa3e9c07e7d1

--- _nuxt/static/1628009593/blog/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog", {data:[{articles:[],_img:{"/_ipx/freeasinbeer.png?w=400":"\u002F_nuxt\u002Fimage\u002F48e788.png"}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1628353052/products/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628353052",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproducts",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7270a1fd"}},_img:{"/_ipx/freeasinbeer_dark.png?w=400":"\u002F_nuxt\u002Fimage\u002F59b950.png"}}}(null));
